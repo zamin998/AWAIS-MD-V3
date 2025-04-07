76cebe87796fce66659d316a0d9b92903840699d
AWAIS <awaisontop346@gmail.com>
2025-04-08 00:41:27 +0500

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -9,7 +9,7 @@ const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, 
 cmd({
     pattern: "yts",
     alias: ["ytsearch"],
-    use: '.yts sameer kutti',
+    use: '.yts ',
     react: "🔎",
     desc: "Search and get details from youtube.",
     category: "search",
