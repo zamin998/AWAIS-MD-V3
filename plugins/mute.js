aad8b83e81549c7856634e1db39a33ca522051fe
AWAIS <awaisontop346@gmail.com>
2025-04-08 00:21:09 +0500

    Update mute.js

--- plugins/mute.js
+++ plugins/mute.js
@@ -2,7 +2,7 @@ const config = require('../config')
 const {cmd , commands} = require('../command')
 cmd({
     pattern: "mute",	
-    alias: ["lock"],
+    alias: ["lock","lock2"],
     react: "🔒",
     desc: "mute group.",
     category: "group",
