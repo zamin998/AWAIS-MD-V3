5e0407d78744eb20ef6dbe410de22f4b8cd2668a
AWAIS <awaisontop346@gmail.com>
2025-04-11 08:50:52 +0500

    Update env.js

--- plugins/env.js
+++ plugins/env.js
@@ -47,7 +47,7 @@ async (conn, mek, m, { from, quoted, reply }) => {
         await conn.sendMessage(
             from,
             {
-                image: { url: 'https://i.imgur.com/XM8Vadf.jpeg' }, // Image URL
+                image: { url: 'https://github.com/Awais-star-a11y/TESTING-REPO/raw/refs/heads/main/IMG-20250409-WA0093.jpg' }, // Image URL
                 caption: envSettings,
                 contextInfo: {
                     mentionedJid: [m.sender],
