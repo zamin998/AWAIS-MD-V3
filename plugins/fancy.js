ec4aac8eb7da6ea45bd7344a8b179f44f5fe4f7a
AWAIS <awaisontop346@gmail.com>
2025-05-09 11:59:30 +0500

    Update fancy.js

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -22,7 +22,7 @@ cmd({
     }
 
     const fonts = response.data.result.map(item => `*${item.name}:*\n${item.result}`).join("\n\n");
-    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *Powered by AWAIS MD*`;
+    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *POWDERED BY AWAIS MD*`;
 
     await conn.sendMessage(from, { text: resultText }, { quoted: m });
   } catch (error) {
